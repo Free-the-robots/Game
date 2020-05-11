6377e2d131b4c545d269b9a3cea2582d21e734b0
Ulysse Vautier <ulysse.vautier@gmail.com>
2020-05-11 14:40:22 +0100

    Added data info on scriptable objects

---
+++ Assets/Scripts/MobileScripts/MaterialsObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mobile
+{
+    [SerializeField]
+    [CreateAssetMenu(fileName = "UserMaterials", menuName = "Mobile/Data", order = 0)]
+    public class MaterialsObject : ScriptableObject
+    {
+        int energy = 100;
+        int coins = 0;
+    }
+}
---
+++ Assets/Scripts/MobileScripts/MobileInfoObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Mobile
+{
+    [CreateAssetMenu(fileName = "UserMaterials", menuName = "Mobile/InfoData", order = 1)]
+    public class MobileInfoObject : ScriptableObject
+    {
+
+    }
+}

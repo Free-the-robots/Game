9ee553e2cba30b920e954dc91a8b5f98f0811022
Ulysse Vautier <ulysse.vautier@gmail.com>
2020-05-14 17:52:49 +0100

    Added animations to menus

---
+++ Assets/Scripts/Common/FillUIParent.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FillUIParent : MonoBehaviour
+{
+    public float time = 1f;
+    public Transform parent;
+    float t = 0f;
+    private RectTransform parentRect;
+    private RectTransform currentRect;
+    public bool width = true;
+    private float w = 0f;
+    public bool height = false;
+    private float h = 0f;
+
+    public UnityEvent Response;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        t = 0f;
+        parentRect = parent.GetComponent<RectTransform>();
+        currentRect = GetComponent<RectTransform>();
+        if (width)
+            w = parentRect.rect.width;
+        else
+            w = currentRect.sizeDelta.x;
+
+        if (height)
+            h = parentRect.rect.height;
+        else
+            h = currentRect.sizeDelta.y;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        float tw = 1f;
+        float th = 1f;
+        if (width)
+            tw = t / time;
+        if (height)
+            th = t / time;
+        if(t > time)
+        {
+            currentRect.sizeDelta = new Vector2(w, h);
+            tw = 1f;
+            th = 1f;
+
+            Response.Invoke();
+
+            enabled = false;
+        }
+        currentRect.sizeDelta = new Vector2(w * tw, h * th);
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Common/LerpAlphaWhenEnabled.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class LerpAlphaWhenEnabled : MonoBehaviour
+{
+    float t = 0f;
+    public float time = 1f;
+    Image imageSprite;
+
+    public UnityEvent Response;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        imageSprite = GetComponent<Image>();
+        t = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        imageSprite.color = new Color(imageSprite.color.r, imageSprite.color.g, imageSprite.color.b, t / time);
+        if(t > time)
+        {
+            Response.Invoke();
+            enabled = false;
+        }
+    }
+}

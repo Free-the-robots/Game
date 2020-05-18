708552def8fd0d35ce361a190af6580d5a626d89
Ulysse Vautier <ulysse.vautier@gmail.com>
2020-05-18 17:28:44 +0100

    Simplified Prefab and Buttons

--- Assets/Scripts/Common/EnableClusterPlanet.cs
+++ Assets/Scripts/Common/EnableClusterPlanet.cs
@@ -6,8 +6,11 @@ public class EnableClusterPlanet : MonoBehaviour
 {
     public FillUIParent line;
     public FillUIParent percentage;
+    public CanvasGroupFade planetInfo;
     public List<PlanetEnabler> planets = new List<PlanetEnabler>();
-    public List<GameObject> planet3D = new List<GameObject>();
+    public GameObject cluster;
+
+    public Camera clusterCamera;
     // Start is called before the first frame update
     public void enableAll()
     {
@@ -28,5 +31,44 @@ public class EnableClusterPlanet : MonoBehaviour
     {
         line.enabled = true;
         percentage.enabled = true;
+    }
+
+    public void FocusOnPlanet(int i)
+    {
+        LerpToWhenEnabled lerp = clusterCamera.GetComponent<LerpToWhenEnabled>();
+        CameraOrthoPerspLerp ortho = clusterCamera.GetComponent<CameraOrthoPerspLerp>();
+
+        lerp.setToTransform(cluster.transform.GetChild(i));
+        lerp.setOffsetZTo(-cluster.transform.GetChild(i).localScale.x);
+        ResetFills();
+
+        planetInfo.enabled = true;
+        lerp.enabled = true;
+        ortho.enabled = true;
+        cluster.transform.GetChild(i).GetChild(0).gameObject.SetActive(true);
+    }
+
+    public void FocusBlackPlanet()
+    {
+        LerpToWhenEnabled lerp = clusterCamera.GetComponent<LerpToWhenEnabled>();
+        CameraOrthoPerspLerp ortho = clusterCamera.GetComponent<CameraOrthoPerspLerp>();
+        enablePercentage();
+
+        planetInfo.gameObject.SetActive(true);
+        planetInfo.enabled = true;
+        lerp.enabled = true;
+        ortho.enabled = true;
+        foreach(Transform planet in cluster.transform)
+        {
+            if(planet.childCount > 0)
+                planet.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+
+    public void CloseCluster()
+    {
+        ResetFills();
+        GetComponent<CanvasGroupFade>().enabled = true;
+        cluster.gameObject.SetActive(false);
     }
 }

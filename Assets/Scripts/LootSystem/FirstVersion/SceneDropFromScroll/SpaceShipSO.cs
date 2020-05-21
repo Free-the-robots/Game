98c075b89a02181c5f51005ac71e8d6774c88ee4
mc <mc@bluefrogrobotics.com>
2020-05-21 22:38:39 +0200

    [LOOTTABLE] scene test with summon

---
+++ Assets/Scripts/LootSystem/FirstVersion/SceneDropFromScroll/SpaceShipSO.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Spaceship", menuName = "Spaceship Data Test LootTable", order = 51)]
+public class SpaceShipSO : ScriptableObject
+{
+    [SerializeField]
+    private string Name;
+    [SerializeField]
+    private int Stars;
+    [SerializeField]
+    private GameObject SpaceshipObject;
+
+    public string SpaceshipName
+    {
+        get { return Name; }
+    }
+
+    public int SpaceshipStars
+    {
+        get { return Stars; }
+    }
+
+    public GameObject SpaceshipGO
+    {
+        get { return SpaceshipObject; }
+    }
+}
---
+++ Assets/Scripts/LootSystem/FirstVersion/SceneDropFromScroll/SummonSystem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SummonSystem : MonoBehaviour
+{
+    [SerializeField]
+    private GenericLootDropTableSO DropTableSO;
+    [SerializeField]
+    private Text NameSummonedItem;
+    [SerializeField]
+    private Text NumberStars;
+
+    private List<GenericLootDropItemSO> DropItem;
+
+    private SpaceShipSO SpaceshipScriptableObject;
+
+    private GameObject SummonedSpaceship;
+
+    private void OnValidate()
+    {
+        //DropTableSO.ValidateTable();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        DropItem = new List<GenericLootDropItemSO>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Summon()
+    {
+        Debug.Log("Summon");
+        SpawnSpaceship();
+    }
+
+    private void SpawnSpaceship()
+    {
+        ClearUI();
+        DropTableSO.ValidateTable();
+        DropItem = DropTableSO.ListDropItem();
+        Debug.Log("DROP ITEM COUNT : " + DropItem.Count);
+        SpaceshipScriptableObject = (SpaceShipSO)DropItem[0].Item;
+        SummonedSpaceship = Instantiate(SpaceshipScriptableObject.SpaceshipGO);
+        SummonedSpaceship.transform.position = new Vector3(400F, 240F, -54F);
+        NameSummonedItem.text = SpaceshipScriptableObject.SpaceshipName;
+        for (int i = 0; i < SpaceshipScriptableObject.SpaceshipStars; ++i)
+            NumberStars.text += "*";
+    }
+
+    private void ClearUI()
+    {
+        DropItem.Clear();
+        NameSummonedItem.text = "";
+        NumberStars.text = "";
+        if (SummonedSpaceship != null)
+            Destroy(SummonedSpaceship);
+
+    }
+}
